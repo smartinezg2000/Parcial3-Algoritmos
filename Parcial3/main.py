ac6895055cba81b32b20e84acf86b14e2eee880d
smartinezg2000 <smartinezg@eafit.edu.co>
2024-05-11 16:30:32 -0500

    .

--- Parcial3/main.py
+++ Parcial3/main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+        else:
+            current_node = self.root
+            while True:
+                if value < current_node.value:
+                    if current_node.left is None:
+                        current_node.left = new_node
+                        return self
+                    current_node = current_node.left
+                else:
+                    if current_node.right is None:
+                        current_node.right = new_node
+                        return self
+                    current_node = current_node.right
+
+    def lookup(self, value):
+        if self.root is None:
+            return False
+        current_node = self.root
+        while current_node:
+            if value < current_node.value:
+                current_node = current_node.left
+            elif value > current_node.value:
+                current_node = current_node.right
+            else:
+                return current_node
+        return False
+
+    def preorder(self, node):
+
+        if node:
+            print(node.dato)
+            self.preorder(node.left)
+            self.preorder(node.right)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+tree = BinarySearchTree()
+tree.insert(4)
+tree.insert(3)
+tree.insert(7)
+tree.insert(2)
+tree.insert(1)
+print(tree.preorder())
