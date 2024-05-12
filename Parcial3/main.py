5856966eb5adfb1f966053e3099fa8e512548c46
caro0616 <lcobandof@eafit.edu.co>
2024-05-12 15:32:52 -0500

    codigo a medias

--- Parcial3/main.py
+++ Parcial3/main.py
@@ -1,57 +1,80 @@
 class Node:
-    def __init__(self, value):
+    def __init__(self,key, value):
+        self.key = key
         self.left = None
         self.right = None
         self.value = value
+        self.height = 1
 
-class BinarySearchTree:
+class AVLTree:
     def __init__(self):
         self.root = None
 
-    def insert(self, letter, code):
-        if self.root is None:
-            self.root = Node(letter)
+    def insert(self, node, key, value):
+        if not node:
+            return TreeNode(key, value)
+        if key < node.key:
+            node.left = self._insert(node.left, key, value)
+        elif key > node.key:
+            node.right = self._insert(node.right, key, value)
         else:
-            current_node = self.root
-            for symbol in code:
-                if symbol == ".":
-                    if current_node.left is None:
-                        current_node.left = Node(letter)
-                    current_node = current_node.left
-                elif symbol == "-":
-                    if current_node.right is None:
-                        current_node.right = Node(letter)
-                    current_node = current_node.right
+            node.value = value
+            return node
 
-    def translate(self,code):
+        node.height = 1 + max(self._get_height(node.left), self._get_height(node.right))
 
-        current = self.root
-        for character in code:
-            if character == '.':
-                current = current.left
-            else:
-                current = current.right
-        return current.value
+        balance = self._get_balance(node)
 
+       #rotaciones para equilibrar el arbol
 
+        if balance > 1 and key < node.left.key:
+            return self._rotate_right(node)
+        if balance < -1 and key > node.right.key:
+            return self._rotate_left(node)
+        if balance > 1 and key > node.left.key:
+            node.left = self._rotate_left(node.left)
+            return self._rotate_right(node)
+        if balance < -1 and key < node.right.key:
+            node.right = self._rotate_right(node.right)
+            return self._rotate_left(node)
 
+        return node
 
+    def _get_height(self, node):
+        if not node:
+            return 0
+        return node.height
 
-tree = BinarySearchTree()
-tree.insert('e','.')
-tree.insert('i ','.')
-tree.insert('a ','-')
-tree.insert('s','..')
-tree.insert('v','..-')
-# tree.insert('','')
-# tree.insert('','')
-# tree.insert('','')
-# tree.insert('','')
-# tree.insert('','')
-# tree.insert('','')
-# tree.insert('','')
-# tree.insert('r ','-.')
+    def _get_balance(self, node):
+        if not node:
+            return 0
+        return self._get_height(node.left) - self._get_height(node.right)
 
+    def _rotate_right(self, z):
+        y = z.left
+        T3 = y.right
+
+        y.right = z
+        z.left = T3
+
+        z.height = 1 + max(self._get_height(z.left), self._get_height(z.right))
+        y.height = 1 + max(self._get_height(y.left), self._get_height(y.right))
+
+        return y
+
+    def _rotate_left(self, z):
+        y = z.right
+        T2 = y.left
+
+        y.left = z
+        z.right = T2
+
+        z.height = 1 + max(self._get_height(z.left), self._get_height(z.right))
+        y.height = 1 + max(self._get_height(y.left), self._get_height(y.right))
+
+        return y
+
+<<<<<<< HEAD
 print(tree.translate('..-'))
 print(tree.root.left.left.value)
 
@@ -62,3 +85,5 @@ codigo_morse = {
 }
 
 
+=======
+>>>>>>> 2836755 (codigo a medias)
